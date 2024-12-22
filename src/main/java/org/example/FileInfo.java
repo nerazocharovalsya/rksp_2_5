4403218f9ff1c54ff86ff02ac8e1de4f223984f3
Dmitry <budima2003@mail.ru>
2024-12-22 20:05:06 +0300

    Initial commit

---
+++ src/main/java/org/example/FileInfo.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class FileInfo {
+    private String filename;
+    private String url;
+}
---
+++ src/main/java/org/example/Pr5Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Pr5Application.class, args);
+    }
+}
